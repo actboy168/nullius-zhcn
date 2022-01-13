b00247f54a034d98e5408235aaf35f095451e9b6
actboy168 <actboy168@gmail.com>
2022-01-13 17:16:48 +0800

    更新发布脚本

--- publish.lua
+++ publish.lua
@@ -8,18 +8,33 @@ local version = (function()
     error 'Cannot found version in info.json.'
 end)()
 
-local archive_name = ("nullius_%s.zip"):format(version)
+local function copyfile(from, to)
+    os.execute(("powershell -NonInteractive -Command Copy-Item -Path %s -Destination %s -Recurse"):format(from, to))
+end
 
-os.remove(archive_name)
-
-local args = {
-    "7z", "-y", "a",
-    archive_name,
+local filelist = {
     "locale",
     "changelog.txt",
     "info.json",
     "LICENSE",
-    "README.md"
+    "README.md",
+}
+
+local name = ("nullius-zhcn_%s"):format(version)
+
+for _, file in ipairs(filelist) do
+    copyfile(file, name.."/"..file)
+end
+
+local archive_name = name..".zip"
+
+os.remove(archive_name)
+
+local args = {
+    "7z", "a",
+    "-y", "-sdel",
+    archive_name,
+    name,
 }
 local command = table.concat(args, " ")
 print(command)
